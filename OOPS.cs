c04bab71bdcd3663cef89385bcb12adf41ac0f97
PallaviKatari <pallavikatari@gmail.com>
2024-07-17 22:13:05 +0530

    commit_17.07.24

---
+++ OOPS.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    /// <summary>
+    /// OOPS - Object Oriented Programming System/Structure
+    /// Class,Objects,Abtraction,Encapsulation,Polymorphism,Inheritance
+    /// </summary>
+    internal class OOPS
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Demo Method");
+        }
+        static void Main(string[] args)
+        {
+            OOPS oops = new OOPS();
+            oops.Demo();
+            Encapsulation encapsulation = new Encapsulation();
+            encapsulation.Id = 10;
+            //encapsulation.Name = "Jane";
+            encapsulation.AccessSpecifiers();
+            Console.WriteLine(encapsulation.Id);
+            Console.ReadLine();
+
+        }
+    }
+
+    internal class Encapsulation
+    {
+        private int id; // default access specifier for a variable - private
+        private string name = "John";
+        /// <summary>
+        /// public,private,internal,protected(inheritance),internal protected
+        /// </summary>
+        public void AccessSpecifiers()
+        {
+            Console.WriteLine(id);
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
---
+++ Polymorphism.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    /// <summary>
+    /// Polymorphism - Many Forms of the same method
+    /// Method Overloading - compile time
+    /// Method Overriding - runtime
+    /// </summary>
+    internal class Polymorphism
+    {
+        /// <summary>
+        /// Function/Method signature - diff in no of parameters,datatype,sequence
+        /// </summary>
+        /// <param name="a"></param>
+        public void Add(int a)
+        {
+            Console.WriteLine(a);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public void Add(int a, int b, int c)
+        {
+            Console.WriteLine(a + b+c);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        public void Add(float a)
+        {
+            Console.WriteLine("Float:"+a);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Add(int a, float b)
+        {
+            Console.WriteLine(a + b);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Add(float a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+            Polymorphism polymorphism = new Polymorphism();
+            polymorphism.Add(10);
+            polymorphism.Add(10, 20);
+            polymorphism.Add(10, 20, 30);
+            polymorphism.Add(12.3f);
+            polymorphism.Add(1, 1.2f);
+            Console.ReadLine();
+        }
+    }
+}
